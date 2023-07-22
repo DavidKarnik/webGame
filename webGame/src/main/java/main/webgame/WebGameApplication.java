2b60120d3fd76f851cfdeed25950d78abf42c807
David_Karnik <david.karnik@tul.cz>
2023-07-22 14:39:10 +0200

    first commit

---
+++ webGame/src/main/java/main/webgame/WebGameApplication.java
@@ -0,0 +1,13 @@
+package main.webgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebGameApplication.class, args);
+	}
+
+}
---
+++ webGame/src/test/java/main/webgame/WebGameApplicationTests.java
@@ -0,0 +1,13 @@
+package main.webgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
