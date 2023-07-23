4d57117cd9a2e044f4bfd3b7e5114464170a45d5
David_Karnik <david.karnik@tul.cz>
2023-07-23 20:12:02 +0200

    Edit target.component behavior

--- angularclient/src/app/target.component.ts
+++ angularclient/src/app/target.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
-import { ScoreService } from './score.service';
+import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
+import {ScoreService} from './score.service';
 
 @Component({
   selector: 'app-target',
@@ -9,37 +9,47 @@ import { ScoreService } from './score.service';
 export class TargetComponent {
   score = 0;
   miss = 0
+  amouseX = 0;
+  amouseY = 0;
 
-  targetPosition = { x: 100, y: 100 }; // Počáteční pozice terče
-  targetRadius: number = 15; // Poloměr terče
-  @ViewChild('target') targetRef!: ElementRef;
+  targetPosition = {x: 100, y: 100}; // Počáteční pozice terče
 
-  constructor(private scoreService: ScoreService) { }
+  constructor(private scoreService: ScoreService) {
+  }
+
 
   @HostListener('document:click', ['$event'])
   onClick(event: MouseEvent) {
-    // Zjistíme, jestli byl kliknut na terč (pomocí offsetTop a offsetLeft)
-    const targetElement = this.targetRef.nativeElement;
     const mouseX = event.clientX;
     const mouseY = event.clientY;
 
+    // Umístění X a Y relativně k oknu prohlížeče
+    const offsetTarget = 30;
+
+    this.amouseX = mouseX;
+    this.amouseY = mouseY;
+
+    const targetPositionX = this.targetPosition.x;
+    const targetPositionY = this.targetPosition.y;
+
     if (
-      mouseX >= targetElement.offsetLeft &&
-      mouseX <= targetElement.offsetLeft + targetElement.offsetWidth &&
-      mouseY >= targetElement.offsetTop &&
-      mouseY <= targetElement.offsetTop + targetElement.offsetHeight
+      mouseX >= targetPositionX - offsetTarget &&
+      mouseX <= targetPositionX + offsetTarget &&
+      mouseY >= targetPositionY - offsetTarget &&
+      mouseY <= targetPositionY + offsetTarget
     ) {
       // Kliknutí na terč
       this.onClickTarget();
     } else {
       // Kliknutí mimo terč
       this.onMiss();
+      this.moveTarget();
     }
   }
 
   onClickTarget() {
     this.score += 1;
-    this.scoreService.incrementScore(10);
+    this.scoreService.incrementScore(1);
     this.moveTarget(); // Při kliknutí na terč se přesune na novou pozici
   }
 
@@ -49,32 +59,21 @@ export class TargetComponent {
   }
 
   moveTarget() {
-    const maxX = window.innerWidth - this.targetRadius * 2; // Maximální hodnota pro souřadnici x
-    const maxY = window.innerHeight - this.targetRadius * 2; // Maximální hodnota pro souřadnici y
+    const maxX = window.innerWidth; // Maximální hodnota pro souřadnici x
+    const maxY = window.innerHeight; // Maximální hodnota pro souřadnici y
+    const minX = 0;
+    const minY = 0;
+    // const maxX = 1600;
+    // const maxY = 700;
 
     // Generujeme náhodné souřadnice pro novou pozici terče
-    const newTargetX = Math.floor(Math.random() * maxX);
-    const newTargetY = Math.floor(Math.random() * maxY);
+    const newTargetX = Math.floor(Math.random() * (maxX - minX + 1)) + minX;
+    const newTargetY = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
 
-    // Přesuneme terč na novou pozici pouze pokud bylo kliknuto na terč
-    if (this.isTargetClicked(newTargetX, newTargetY)) {
-      this.targetPosition.x = newTargetX;
-      this.targetPosition.y = newTargetY;
-
-      // Přesuneme terč na novou pozici pomocí CSS transformace
-      this.targetRef.nativeElement.style.transform = `translate(${this.targetPosition.x}px, ${this.targetPosition.y}px)`;
-    }
-  }
-
-  private isTargetClicked(newX: number, newY: number): boolean {
-    // Zjistíme, jestli bylo kliknuto na terč (pomoci offsetTop a offsetLeft)
-    const targetElement = this.targetRef.nativeElement;
-
-    return (
-      newX >= targetElement.offsetLeft &&
-      newX <= targetElement.offsetLeft + targetElement.offsetWidth &&
-      newY >= targetElement.offsetTop &&
-      newY <= targetElement.offsetTop + targetElement.offsetHeight
-    );
+    // Přesuneme terč na novou pozici
+    this.targetPosition.x = newTargetX;
+    this.targetPosition.y = newTargetY;
+    // console.log(`newTargetX: ${newTargetX}`);
+    // console.log(`newTargetY: ${newTargetY}`);
   }
 }
