e11496f55b79c561639eff582c9fa28a35d4ef5c
David_Karnik <david.karnik@tul.cz>
2023-08-21 20:47:30 +0200

    Add findAll

--- webGame/src/main/java/main/webgame/controller/LeaderboardController.java
+++ webGame/src/main/java/main/webgame/controller/LeaderboardController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/api") // prefix need to add to the WebSecurityConfig.java
 public class LeaderboardController {
@@ -27,10 +29,16 @@ public class LeaderboardController {
      * Load Leaderboard
      * @return - Array of Player[]
      */
-    @GetMapping("/leaderboard")
-    public Player[] showLeaderboard() {
+    @GetMapping("/save")
+    public Player[] saveDebugging() {
         Player plr = new Player("Player1",123);
         userRepository.save(new Player("Player1",123));
         return null;
     }
+
+    @GetMapping("/leaderboard")
+    public List<Player> showLeaderboard() {
+        return userRepository.findAll();
+    }
+
 }
