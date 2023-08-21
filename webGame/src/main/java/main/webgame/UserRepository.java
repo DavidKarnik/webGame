39f004c6bc721620a20eebd9264e5f94fc7f55b6
David_Karnik <david.karnik@tul.cz>
2023-08-21 20:47:56 +0200

    comment

--- webGame/src/main/java/main/webgame/UserRepository.java
+++ webGame/src/main/java/main/webgame/UserRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// JPA provides you with the CRUD operations
 @Repository
 public interface UserRepository extends JpaRepository<Player, Integer> {
+
+    // can add custom query
 
 }
